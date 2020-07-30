96645d187392b12e224e1087f40b3d6d1ac4f62e
ason <ahsan.virani08@gmail.com>
2020-07-30 21:49:41 +1000

    Fixed?

--- GameView.c
+++ GameView.c
@@ -823,17 +823,17 @@ PlaceId *GvGetReachableByType(GameView gv, Player player, Round round,
 	else {
 		int RailMoves = distanceRail(round, player); 
 		while(c != NULL) {
-			if(rail) {
-				if(c->type == RAIL) {
-					if(RailMoves > 0) {
-						j = addRailConnection_wrapper(gv, c->p, RailMoves, j);
-					}
-					else {
-					c = c->next;
-					continue;
-					}
+		
+			if(c->type == RAIL && rail) {
+				if(RailMoves > 0) {
+					j = addRailConnection_wrapper(gv, c->p, RailMoves, j);
+				}
+				else {
+				c = c->next;
+				continue;
 				}
 			}
+		
 			else if(!InArray(c->p)){
 				if(road) {
 					addRoadConnection(c->p, j);
