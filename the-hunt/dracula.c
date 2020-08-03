4fe83f2a2947326684d699f1d62ec20e0ca3800a
ason <ahsan.virani08@gmail.com>
2020-08-03 21:19:31 +1000

    trying

--- the-hunt/dracula.c
+++ the-hunt/dracula.c
@@ -8,56 +8,181 @@
 // 2020-07-10	v3.0	Team Dracula <cs2521@cse.unsw.edu.au>
 //
 ////////////////////////////////////////////////////////////////////////
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
 
 #include "dracula.h"
 #include "DraculaView.h"
 #include "Game.h"
 
-// Registers best play for when Dracula has not made a move
-void draculaStarting(DraculaView dv);
-// Registers Teleport when Dracula has no valid moves
-void draculaTeleport();
-// AI to register best play for Dracula
-void draculaAI(DraculaView dv, PlaceId currentLocation, PlaceId *validMoves);
+#define MAX_LOCATION 100
+#define DRAC_REACH 10
+
+PlaceId huntersLoc[MAX_LOCATION];
+int size;
+
+// Static Function Declarations
+static void huntersReach(DraculaView);
+static int InArray(DraculaView, Player);
+static void updateHuntersReach(DraculaView, Player);
+static int DraculaAI(PlaceId *validMoves, PlaceId draculaReac[], int numReturnedMoves);
+static void draculaStarting(DraculaView);
+static void draculaTeleport();
+
 
 
 void decideDraculaMove(DraculaView dv)
 {
-    int numReturnedMoves;
-    PlaceId *moves = DvGetValidMoves(dv, &numReturnedMoves);
-    PlaceId currentLocation = DvGetPlayerLocation(dv, PLAYER_DRACULA);
 
-    // if Dracula has not made a move
-    if (currentLocation == NOWHERE) {
+	PlaceId draculaReach[DRAC_REACH];
+
+	// initialise the huntersLoc array with NOWEHRE
+	int i;	
+	for(i = 0; i < MAX_LOCATION; i++)
+		huntersLoc[i] = NOWHERE;
+	size = 0;
+	// initialise the draculaReach array with NOWHERE
+	for(i = 0; i < DRAC_REACH; i++)
+		draculaReach[i] = NOWHERE;
+
+	// Fills the huntersLoc array with current and reachable locations of all hunters
+	huntersReach(dv);
+
+	// If Round 0 Play anything for testing
+	if(DvGetRound(dv) == 0) {
         draculaStarting(dv);
-    // if Dracula has no valid moves, teleport
-    } else if (numReturnedMoves == 0) {
-        draculaTeleport();
-    // otherwise register the first valid move to meet time-constraint
-    // and implement AI to select the best move
-    } else {
-	    registerBestPlay(placeIdToAbbrev(moves[0]), "Mwahahahaha");
-        draculaAI(dv, currentLocation, moves);
-    }
-    free(moves);
+		return;
+	}
 
-    return;
+
+	// Takes Valid Moves of Dracula and returns array that excludes locations of huntersReach
+	int numReturnedMoves = -1;
+	PlaceId *Moves = DvGetValidMoves(dv, &numReturnedMoves);
+
+	// Means teleported
+	if(numReturnedMoves == 0) {
+		draculaTeleport();
+		free(Moves);
+		return;
+	}
+	// gives the first element in best moves array
+	int nMoves = DraculaAI(Moves, draculaReach, numReturnedMoves);
+	if(nMoves > 0) {
+		registerBestPlay(placeIdToAbbrev(draculaReach[0]), "Mwahahahaha");
+		free(Moves);	
+		return;
+	}		
+	registerBestPlay(placeIdToAbbrev(Moves[0]), "Mwahahahaha");
+	free(Moves);
+}
+
+// Fills the array with current location of hunters and places reachable
+static
+void huntersReach(DraculaView dv)
+{
+	assert(dv != NULL);
+
+	// insert hunters current location in the array
+	huntersLoc[0] = DvGetPlayerLocation(dv, 0);
+	size++;
+	if(!InArray(dv, 1)) {
+		huntersLoc[1] = DvGetPlayerLocation(dv, 1);
+		size++;
+	}
+	if(!InArray(dv, 2)) {
+		huntersLoc[2] = DvGetPlayerLocation(dv, 2);
+		size++;
+	}
+	if(!InArray(dv, 3)) {
+		huntersLoc[3] = DvGetPlayerLocation(dv, 3);
+		size++;
+	}
+	// Insert places reachable by the hunters in the array
+	updateHuntersReach(dv, 0);
+	updateHuntersReach(dv, 1);
+	updateHuntersReach(dv, 2);
+	updateHuntersReach(dv, 3);
+}
+
+static
+int InArray(DraculaView dv, Player player)
+{
+	assert(dv != NULL);
+		
+	if(player == 1) {
+		if(huntersLoc[0] != DvGetPlayerLocation(dv, 1))
+			return 0;
+		return 1;
+	}	
+
+	else if(player == 2) {
+		if(huntersLoc[0] != DvGetPlayerLocation(dv, 2) && 
+			huntersLoc[1] != DvGetPlayerLocation(dv, 2))
+			return 0;
+		return 1;
+	}
+	
+	if(huntersLoc[0] != DvGetPlayerLocation(dv, 3) && 
+		huntersLoc[1] != DvGetPlayerLocation(dv, 3) && 
+		huntersLoc[2] != DvGetPlayerLocation(dv, 3))
+		return 0;
+	return 1;
+}
+
+static
+void updateHuntersReach(DraculaView dv, Player player)
+{
+	int numReturnedLocs = 0;
+	PlaceId *reachable = DvWhereCanTheyGo(dv, player, &numReturnedLocs);
+	
+	int i, j;
+	for(i = 0; i < numReturnedLocs; i++) {
+		for(j = 0; j < size; j++) {
+			if(huntersLoc[j] == reachable[i])
+				break;
+		}
+		if(size == j) {
+			huntersLoc[size] = reachable[i];
+			size++;
+		}
+	}
+	free(reachable);
+}
+
+// Takes valid moves for the Dracula and excludes those in Hunters Reach
+// If no such move i.e. Dracula trapped or no valid moves then returns 0
+static
+int DraculaAI(PlaceId *validMoves, PlaceId draculaReach[], int numReturnedMoves)
+{
+	int len = 0;
+	int i, j;
+	for(i = 0; i < numReturnedMoves; i++) {
+		for(j = 0; j < size; j++) {
+			if(huntersLoc[j] == validMoves[i])
+				break;
+		}
+		if(size == j) {
+			draculaReach[len] = validMoves[i];
+			len++;
+		}		
+	}	
+	return len;
 }
 
 // Registers best play for when Dracula has not made a move
+// Make sure to start where the Hunters not
+static
 void draculaStarting(DraculaView dv) {
     // TODO: Replace this with something better!
 	registerBestPlay("CD", "Mwahahahaha");
 }
 
 // Registers Teleport when Dracula has no valid moves
+static
 void draculaTeleport() {
     registerBestPlay("TP", "Mwahahahaha");
     return;
 }
 
-// AI to register best play for Dracula
-void draculaAI(DraculaView dv, PlaceId currentLocation, PlaceId *validMoves) {
-
-    return;    
-}
---
+++ the-hunt/hunter.c
@@ -0,0 +1,151 @@
+////////////////////////////////////////////////////////////////////////
+// COMP2521 20T2 ... the Fury of Dracula
+// hunter.c: your "Fury of Dracula" hunter AI.
+//
+// 2014-07-01	v1.0	Team Dracula <cs2521@cse.unsw.edu.au>
+// 2017-12-01	v1.1	Team Dracula <cs2521@cse.unsw.edu.au>
+// 2018-12-31	v2.0	Team Dracula <cs2521@cse.unsw.edu.au>
+// 2020-07-10	v3.0	Team Dracula <cs2521@cse.unsw.edu.au>
+//
+////////////////////////////////////////////////////////////////////////
+
+#include "Game.h"
+#include "hunter.h"
+#include "HunterView.h"
+#include "GameView.h"
+#include <stdio.h>
+
+void decideHunterMove(HunterView hv)
+{
+	// Hunter walks around in a circle
+    // Pending hunterview to be fixed to implement AI
+
+    int round = HvGetRound(hv);
+    int player = HvGetPlayer(hv);
+    printf("round: %d\n", round);
+    // Check if player is at ST_JOSEPH_AND_ST_MARY (killed) stay
+    if (HvGetPlayerLocation(hv, player) == ST_JOSEPH_AND_ST_MARY){
+        registerBestPlay("JM", "None");
+        return;
+    }
+
+    switch (round % 39) {
+        case 0:
+            registerBestPlay("BD", "None");
+        break;
+        case 1:
+            registerBestPlay("SZ", "None");
+        break;
+        case 2:
+            registerBestPlay("KL", "None");
+        break;
+        case 3:
+            registerBestPlay("CD", "None");
+        break;
+        case 4:
+            registerBestPlay("GA", "None");
+        break;
+        case 5:
+            registerBestPlay("CN", "None");
+        break;
+        case 6:
+            registerBestPlay("VR", "None");
+        break;
+        case 7:
+            registerBestPlay("BS", "None");
+        break;
+        case 8:
+            registerBestPlay("IO", "None");
+        break;
+        case 9:
+            registerBestPlay("VA", "None");
+        break;
+        case 10:
+            registerBestPlay("SJ", "None");
+        break;
+        case 11:
+            registerBestPlay("ZA", "None");
+        break;
+        case 12:
+            registerBestPlay("MU", "None");
+        break;
+        case 13:
+            registerBestPlay("VE", "None");
+        break;
+        case 14:
+            registerBestPlay("FL", "None");
+        break;
+        case 15:
+            registerBestPlay("RO", "None");
+        break;
+        case 16:
+            registerBestPlay("NP", "None");
+        break;
+        case 17:
+            registerBestPlay("TS", "None");
+        break;
+        case 18:
+            registerBestPlay("MS", "None");
+        break;
+        case 19:
+            registerBestPlay("MR", "None");
+        break;
+        case 20:
+            registerBestPlay("CF", "None");
+        break;
+        case 21:
+            registerBestPlay("TO", "None");
+        break;
+        case 22:
+            registerBestPlay("SR", "None");
+        break;
+        case 23:
+            registerBestPlay("MA", "None");
+        break;
+        case 24:
+            registerBestPlay("SN", "None");
+        break;
+        case 25:
+            registerBestPlay("BB", "None");
+        break;
+        case 26:
+            registerBestPlay("AO", "None");
+        break;
+        case 27:
+            registerBestPlay("IR", "None");
+        break;
+        case 28:
+            registerBestPlay("SW", "None");
+        break;
+        case 29:
+            registerBestPlay("LV", "None");
+        break;
+        case 30:
+            registerBestPlay("MN", "None");
+        break;
+        case 31:
+            registerBestPlay("ED", "None");
+        break;
+        case 32:
+            registerBestPlay("NS", "None");
+        break;
+        case 33:
+            registerBestPlay("AM", "None");
+        break;
+        case 34:
+            registerBestPlay("CO", "None");
+        break;
+        case 35:
+            registerBestPlay("HA", "None");
+        break;
+        case 36:
+            registerBestPlay("BR", "None");
+        break;
+        case 37:
+            registerBestPlay("PR", "None");
+        break;
+        case 38:
+            registerBestPlay("VI", "None");
+        break;
+    }
+}
